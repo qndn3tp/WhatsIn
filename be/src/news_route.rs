b94f102359fbdd74ff437be3a31beb74b2caf78d
jollidah <help0304@naver.com>
2023-10-27 23:00:54 +0900

    modify test struct to News{title: String, body: String}

--- be/src/news_route.rs
+++ be/src/news_route.rs
@@ -1,13 +1,15 @@
 use axum::{Router, routing::get, Json};
-use crate::schemas::out_schemas::TestStruct;
+use crate::schemas::out_schemas::News;
 
 #[utoipa::path( get,  path = "/api/vi/external/test")]
 #[axum_macros::debug_handler]
-pub async fn test_router() -> Json<TestStruct> {
-    Json(TestStruct{ string: "This is a test string!!".into() })
+pub async fn news_router() -> Json<News> {
+    Json(News{ 
+        title: "Gun hae is idiot!!".to_string(), 
+        body: "Reseach which proved Gun hae is idiot has been reported".to_string(),})
 }
 
 pub fn news_routers() -> Router<> {
     Router::new()
-    .route("/test", get(test_router))
+    .route("/test", get(news_router))
 }
