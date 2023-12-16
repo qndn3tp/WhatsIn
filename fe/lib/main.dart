1dafcb4778b9a4bb50fb422a9116ffe6af51e8e2
qndn3tp <qndn3tp@naver.com>
2023-12-17 00:45:57 +0900

    Feat: Receive data and show it on user screen

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -15,11 +15,11 @@ import './widgets/Science.dart';
 import './widgets/Health.dart';
 
 // Get data from server
-Future<List<NewsData>> getNewsData(http.Client client) async {
+Future<List<NewsData>> getNewsData(http.Client client, {String category=""}) async {
   String api_url = dotenv.env['API_URL'] ?? "";                 // Get API_URL from env file and Set api_url
-  String science = dotenv.env['SCIENCE'] ?? "";                 // Choose Category
+  String cat = dotenv.env[category] ?? "";                      // Get Selected category from env file
 
-  final response = await client.get(Uri.parse(api_url + science));        // Send http GET requests to server url and processes the response
+  final response = await client.get(Uri.parse(api_url + cat));        // Send http GET requests to server url and processes the response
 
   if (response.headers['content-type']?.toLowerCase().contains('charset=utf-8') != true) {  // Set encoding charset if it is not set in Content-Type header
     response.headers['content-type'] = 'application/json; charset=utf-8';
