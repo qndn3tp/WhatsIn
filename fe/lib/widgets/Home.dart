8ef79fe8351677201f5ee215ac8c83a6b9d3f979
qndn3tp <qndn3tp@naver.com>
2023-12-29 22:27:04 +0900

    Feat: Update Home page

--- fe/lib/widgets/Home.dart
+++ fe/lib/widgets/Home.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import '../main.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key});
+
+  Future<void> _launchUrl(String url) async{
+    if (!await launchUrl(Uri.parse(url))) {
+      throw Exception('Could not launch');
+    }
+  }
+
+  // Categories
+  final categories = ['GENERAL', 'BUSINESS', 'ENTERTAINMENT', 'SPORTS', 'TECHNOLOGY', 'SCIENCE', 'HEALTH'];
+  final categories_ko = ['정치', '경제', '연예', '스포츠', '기술', '과학', '건강'];
 
   @override
   Widget build(BuildContext context) {
@@ -16,11 +29,13 @@ class Home extends StatelessWidget {
 
         // Main space: News list
         Expanded(
-            child: ListView.builder(
-                itemCount: 7,                         // itemCount: number of tabs
-                itemBuilder: (c, i){
-                  return Container(
-                    // Design property
+          child: ListView.builder(
+              itemCount: categories.length,                         // itemCount: 7 categories in categories list
+              itemBuilder: (c, i){                                  // loop categories list, then receive all category's NewsData
+                return Container(
+                  // Design property
+                    height: 130,
+                    width: 500,
                     margin: EdgeInsets.only(left: 35, top: 20, right: 35, bottom: 0),
                     padding: EdgeInsets.all(20),
                     decoration: BoxDecoration(
@@ -36,29 +51,80 @@ class Home extends StatelessWidget {
                     ),
 
                     // Content
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        // News image
-                        Container(
-                          height: 50,
-                          width: 60,
-                          child:Icon(Icons.image),
-                        ),
-                        //News title, News body
-                        Container(
-                          width: 200,
-                          child: Column(
-                            children: [
-                              Text("기사 제목", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),),
-                              Text("기사 본문", style: TextStyle(fontSize: 15),),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  );}
-            )
+                    child: FutureBuilder<List<NewsData>>(                             // Perform async operation
+                      future: getNewsData(http.Client(), category: categories[i]),
+                      builder: (context, snapshot) {                                  // snapshot: Object that includes async operation's result, state
+
+                        if (snapshot.connectionState == ConnectionState.waiting) {    // UI: when data is loading
+                          return CircularProgressIndicator();
+                        }
+                        else if (snapshot.hasError) {                                 // UI: when error occurs
+                          return Text('Error: ${snapshot.error}');
+                        }
+                        else {                                                        // UI: when data is successfully received
+                          NewsData newsData = snapshot.data![0];
+
+                          String title = newsData.title.length > 50                   // Cut the string to title's maximum length
+                              ? newsData.title.substring(0, 50) + '...'
+                              : newsData.title;
+
+                          String description = newsData.description.length > 50      // Cut the string to description's maximum length
+                              ? newsData.description.substring(0, 50) + '...'
+                              : newsData.description;
+
+                          String url_to_image = newsData.url_to_image;
+
+                          String url_to_article = newsData.url_to_article;
+
+                          // News article
+                          return Container(
+                            child: GestureDetector(
+                              onTap: (){
+                                _launchUrl(url_to_article);                   // Clicked => Attempt to open browser(url_to_article)
+                              },
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  // News image
+                                  Container(
+                                    margin: EdgeInsets.only(left: 0, top: 0,right: 20, bottom: 0),
+                                    height: 200,
+                                    width: 90,
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          children: [
+                                            SizedBox(width: 10,),
+                                            Text(categories_ko[i], style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Color(0xff33484A)),),
+                                          ],
+                                        ),
+                                        SizedBox(height: 5,),
+                                        Image.network(url_to_image)
+                                      ],
+                                    ),
+                                  ),
+                                  // News title, News body
+                                  Container(
+                                    height: 200,
+                                    width: 280,
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        Text(title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
+                                        Text(description, style: TextStyle(fontSize: 13),),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          );
+                        }
+                      },
+                    )
+                );}
+          ),
         )
       ],
     );
